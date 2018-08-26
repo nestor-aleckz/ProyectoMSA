91cd38a6f9bec346a1d6e2d0694c96d093d5eef7
Pamela Palacios <tkpamelapalacios@gmail.com>
2018-08-25 21:22:17 -0600

    Mejoras en el listado y module para editar y crear. Sin guardar data.

--- frontend/app/components/survey/survey.component.ts
+++ frontend/app/components/survey/survey.component.ts
@@ -13,7 +13,8 @@ export class SurveyComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $('#surveysFormModal').modal({show:false});
+    $('#surveysSeeFormModal').modal({show:false});
+    $('#surveysEditFormModal').modal({show:false});
     this.getSurveys();
   }
 
@@ -22,13 +23,24 @@ export class SurveyComponent implements OnInit {
     [
         {surveyIndex: 1, nameSurvey: "Eyespops", benefitSurvey: "Ingreso gratis el 30 de Agosto eyespopland", providerSurvey: "Eyespop S.A." },
         {surveyIndex: 2, nameSurvey: "Grupo Hame 50%", benefitSurvey: "50% de descuento en productos de la marca.", providerSurvey: "Grupo Hame S.A." },
-        {surveyIndex: 3, nameSurvey: "Genba está de feria!", benefitSurvey: "2x1 en tu próxima compra.", providerSurvey: "Genba" }
+        {surveyIndex: 3, nameSurvey: "Genba está de feria!", benefitSurvey: "2x1 en tu próxima compra.", providerSurvey: "Genba" },
+        {surveyIndex: 4, nameSurvey: "Querido, Santa! Navidad Azul", benefitSurvey: "10% de Reintegro en tu proxima compra.", providerSurvey: "Samsung L.A." },
+        {surveyIndex: 5, nameSurvey: "Fecha? Soy Luna en Vivo!", benefitSurvey: "Parqueo gratis en Oakland Mall durante un mes.", providerSurvey: "Diseny Latinoamérica" }
     ]
   }
 
-  crear(){
-    console.log("Solicita crear una nueva encuesta");
-    $('#surveysFormModal').modal("toggle");
+  see(){    
+    console.log("Solicita ver detalle de una nueva encuesta");
+    $('#surveysSeeFormModal').modal("toggle");
+  }
 
+  edit(){    
+    console.log("Solicita editar la encuesta no.-");
+    $('#surveysEditFormModal').modal("toggle");
+  }
+
+  hide(){
+    $('#surveysSeeFormModal').modal({show:false});
+    $('#surveysEditFormModal').modal({show:false});
   }
 }
